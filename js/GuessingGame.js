7c095d176e59aa6354166d3ae4a759e1e293f304
Wyestar <dougmchin@gmail.com>
2017-09-05 01:09:19 -0400

    ggwip

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -2,8 +2,8 @@ function generateWinningNumber() {
   // returns a random number from 1 through 100, not including 100
   // if 0, return 1
   // Math.floor(Math.random() * 100 + 1);
-  var r = Math.floor( Math.random() * 100 );
-  return r + 1;
+  var r = 1 + Math.floor( Math.random() * 100 );
+  return r;
 }
 
 function shuffle(arr) {
@@ -49,6 +49,8 @@ Game.prototype.playersGuessSubmission = function(guess) {
 Game.prototype.checkGuess = function() {
   var difference = this.difference();
   if (this.playersGuess === this.winningNumber) {
+    $("#submit, #hintArray, #hintLowHigh").prop("disabled", true);
+    $("#subtitle").text("Press reset to play again");
     return "You Win!";
   }
   else if (this.pastGuesses.indexOf(this.playersGuess) > -1) {
@@ -57,8 +59,13 @@ Game.prototype.checkGuess = function() {
   else {
     // guess is not the winning number or a repeat guess
     this.pastGuesses.push(this.playersGuess);
+    // change guess list
+    $("#guess-list li:nth-child("+ this.pastGuesses.length +")").text(this.playersGuess);
+
     if (this.pastGuesses.length === 5) {
-      return "You Lose.";
+      $("submit, #hintArray, #hintLowHigh").prop("disabled", true);
+      $("#subtitle").text("Press reset to play again");
+      return "You Lose. The numbers was: " + this.winningNumber;
     }
     else {
       // not at fifth guess yet
@@ -87,7 +94,55 @@ function newGame() {
 Game.prototype.provideHint = function () {
   // generateWinningNumber are two random numbers, this.winningNumber is the winning number
   var hints = shuffle([generateWinningNumber(), generateWinningNumber(), this.winningNumber]);
+  $("#subtitle").text("Potential winning numbers: " + hints.join(" "));
   return hints;
 };
 
-var game = new Game();
+Game.prototype.provideGuideHint = function() {
+  if (this.isLower()) {
+    // guess is lower than winning number
+    $("#subtitle").text("Guess higher...");
+  }
+  else {
+    // guess is higher than winning number
+    $("#subtitle").text("Guess lower...");
+  }
+}
+
+// var game = new Game();
+
+// return changes h1, title
+
+$(document).ready(function() {
+  var game = new Game();
+  $("#submit").click(function(e) {
+    guessPlayerInput(game);
+  })
+  $('#player-input').keypress(function(event) {
+    if (event.which == 13) {
+      guessPlayerInput(game);
+    }
+  })
+})
+
+function guessPlayerInput(gameInstance) {
+  var pg = parseInt( ($("#player-input").val()), 10);
+  var result = gameInstance.playersGuessSubmission(pg);
+  $("#player-input").val("");
+  $("#title").text(result);
+  return game.playersGuessSubmission();
+}
+
+$("#hintArray").click(function() {
+  game.provideHint();
+})
+
+$("#hintLowHigh").click(function() {
+  game.provideGuideHint();
+})
+
+$("#reset").click(function() {
+  $("#title, #subtitle, #guess-list").val("");
+  $("#submit, #hints").prop(disabled, false);
+  var game = newGame();
+})
---
+++ js/application.js
@@ -0,0 +1,2 @@
+// jquery file
+// application.js
